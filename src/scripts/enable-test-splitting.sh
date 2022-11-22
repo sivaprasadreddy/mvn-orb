32e82a957d857e05f24a4f0ca6776da6cecb4129
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-11-22 10:16:37 +0530

    Clone of maven-orb with bug fixes

--- src/scripts/enable-test-splitting.sh
+++ src/scripts/enable-test-splitting.sh
@@ -8,35 +8,17 @@ echo -e "\033[31m\033[4mOrb Notes on Test Splitting\033[0m
         \033[34mDebugging:\033[0m This orb will place all files used to decide tests in .circleci/tests and export it as an artifact for inspection/debugging.
         "
 mkdir -p .circleci/tests/
-echo "PARAM_TEST_PATTERN: $PARAM_TEST_PATTERN"
+
 # generate excluded surefire tests using provided pattern
-circleci tests glob ${PARAM_TEST_DIR}/${PARAM_TEST_PATTERN} | \
+circleci tests glob "${PARAM_TEST_DIR}"/"${PARAM_TEST_PATTERN}" | \
     sed -e "s#^$PARAM_TEST_DIR/\(.*\)\.(java|kt)#\1#" | \
     tr "/" "." > .circleci/tests/surefire_classnames
-echo "generate excluded surefire tests command exit status: $?"
 circleci tests split --split-by=timings --timings-type=classname < .circleci/tests/surefire_classnames > /tmp/this_node_tests
-echo "surefire test split command exit status: $?"
-cat .circleci/tests/surefire_classnames
-echo "------"
-cat /tmp/this_node_tests
-echo "------"
-
 grep -xvf /tmp/this_node_tests < .circleci/tests/surefire_classnames > .circleci/tests/surefire_classnames_ignore_list
-echo "write surefire_classnames_ignore_list command exit status: $?"
 
-echo "PARAM_IT_PATTERN: $PARAM_IT_PATTERN"
 # generate excluded failsafe tests using provided pattern
 circleci tests glob "${PARAM_TEST_DIR}/${PARAM_IT_PATTERN}" | \
     sed -e "s#^$PARAM_TEST_DIR/\(.*\)\.(java|kt)#\1#" | \
     tr "/" "." > .circleci/tests/failsafe_classnames
-echo "generate excluded failsafe tests command exit status: $?"
 circleci tests split --split-by=timings --timings-type=classname < .circleci/tests/failsafe_classnames > /tmp/this_node_it_tests
-echo "failsafe test split command exit status: $?"
-
-cat .circleci/tests/failsafe_classnames
-echo "------"
-cat /tmp/this_node_it_tests
-echo "------"
-
 grep -xvf /tmp/this_node_it_tests < .circleci/tests/failsafe_classnames > .circleci/tests/failsafe_classnames_ignore_list
-echo "write failsafe_classnames_ignore_list command exit status: $?"
